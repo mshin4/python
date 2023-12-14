c7869b78e87eeb40144c4701a1a6cccaae081bac
Michael <mshin4@yahoo.com>
2023-12-13 23:09:52 -0800

    factorial tip

--- tips.py
+++ tips.py
@@ -95,3 +95,24 @@ tripler = myfunc(3)
 print(doubler(11))
 print(tripler(11))
 '''
+
+# factorial function
+
+import math
+import os
+import random
+import re
+import sys
+
+def factorial(n):
+    # Write your code here
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
+
+if __name__ == '__main__':
+
+    n = int(input().strip())
+
+    result = factorial(n)
+    print(result)
