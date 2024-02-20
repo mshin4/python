3c91f1c833151df71e1080349b2139bdcdaf5eaf
Michael <mshin4@yahoo.com>
2024-02-19 23:43:29 -0800

    + todo.py

---
+++ todo.py
@@ -0,0 +1,3 @@
+## TODO Operational Projects: Find duplicate files
+## TODO Operational Projects: Find commond keywords in files like IP address
+## TODO Operational Projects: Use tkinter to create a GUI for above
