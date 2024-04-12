20e5d870b6498a383cd184a2a0cc7216ef8e42ff
sixteen <mshin4@yahoo.com>
2024-04-12 10:50:06 -0700

    +grocery project

---
+++ grocery.py
@@ -0,0 +1,11 @@
+from ingredients import meals
+
+# From dictionary show all the meals to choose from
+
+print(meals.keys())
+
+# while loop user input of meals
+
+# when finished give entire list of ingredients
+
+# sort ingredients by categor such as produce, meat, etc...
---
+++ ingredients.py
@@ -0,0 +1,13 @@
+# TODO
+'''
+add to the dictionary all your meals
+'''
+
+
+meals = {
+    "beef_rice": ["ground_beef", "ginger"]
+}
+
+print(meals["beef_rice"])
+
+
