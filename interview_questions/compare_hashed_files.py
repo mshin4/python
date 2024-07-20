78b25c23115b545ce76ad7a10043949c55fe2ea5
sixteen <mshin4@yahoo.com>
2024-07-19 23:41:09 -0700

    interview questions

---
+++ interview_questions/compare_hashed_files.py
@@ -0,0 +1,14 @@
+### Given the dictionary, output the file names that are identical.
+
+dict = {
+    "file1": "e7d29b6fc7afbffd3829f373a3a30876",
+    "file2": "bad5a62acd5007c3eab9b08ca3f8941f",
+    "file3": "e7d29b6fc7afbffd3829f373a3a30876",
+    "file4": "9b36325abb4a6d8fd577e4676ade182f",
+    "file5": "c111262de69c0ddfdc421d32feafce6c",
+    "file6": "bad5a62acd5007c3eab9b08ca3f8941f",
+    "file7": "e7d29b6fc7afbffd3829f373a3a30876",
+}
+
+dict_result = {}
+
---
+++ interview_questions/find_maximum_repeats.py
@@ -0,0 +1,4 @@
+### TODO Given the list, output the item that repeats the most
+list = [1, 5, 7, 11, 12, 5, 55, 3, 22, 11, 5, 1, 2]
+counter = 0
+number = 0
