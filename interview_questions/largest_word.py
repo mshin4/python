56d564fc19413a7700455f40419cd19d77cc76c1
sixteen <mshin4@yahoo.com>
2024-08-16 20:32:09 -0700

    +descriptions

--- interview_questions/largest_word.py
+++ interview_questions/largest_word.py
@@ -1,3 +1,8 @@
+'''Working with strings
+John loves analysing textual information. He often reads short stories and then tries to find the longest word in such stories. Doing this manually takes a long time so we'd like to help him automate the task. \
+Note: You are provided an example of a short story in the code editor. The example story should not be part of your solution: it is just there to help you.
+Write a function named get_longest_word which accepts a single string argument. The function should return the longest word in the given string. Assume that words in a string can be separated with spaces, commas, new line characters or full stops. This means that abbreviated forms with apostrophes (e.g. I'm) are considered to be a single word. If there is more than one word with the maximum number of characters, return the first such word that exists in the string.
+Example: For input Once I'm awaken, I'll sacrifice your soul to the ruler of darkness. the output should be sacrifice.'''
 
 sample_story = '''Once upon a time, there was a beginner programmer named Alice who was eager to learn Python. She tried to learn from books, but found it difficult to grasp the concepts. One day, she stumbled upon an online course.
 
--- interview_questions/lottery.py
+++ interview_questions/lottery.py
@@ -1,3 +1,12 @@
+'''Let's write a simple function that will help organise lotteries. The function will generate a list of random numbers (to simulate lottery tickets), and it will also choose one number from the generated list (to simulate the winning ticket).
+Write a function named generate_tickets that will accept two integer arguments: ticket_count and max_number. The function should return a tuple with exactly two elements:
+first element: a list of random unique integer numbers in the range from 0 (inclusive) to max_number (exclusive); the number of elements is provided in the ticket_count argument
+second element: one random element from the generated list of numbers
+Calling generate_tickets(5, 10) should generate 5 random unique integers in the range from 0 (inclusive) to 10 (exclusive). An example return value for this invocation could be:
+([2, 8, 9, 3, 0], 8)
+In this case, the random numbers are: 2, 8, 9, 3, 0. The winning number is 8.
+Note: You can assume that the arguments of the function are always correct (i.e. you always get two correct integers as the input arguments).'''
+
 ##TODO using random module, generate a list of random numbers and randomly pick a winning numbef from that list
 
 
